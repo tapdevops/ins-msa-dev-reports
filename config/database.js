e197ddb29c311cb1ba2e2c65e98cdc53f1b12360
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-21 15:31:38 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@
 */
 	module.exports = {
 		production: {
-			url: 'mongodb://dbapp:dbapp123@dbapp.tap-agri.com:27017/s_images?authSource=admin',
+			url: 'mongodb://dbapp:dbapp123@dbapp.tap-agri.com:27017/s_report?authSource=admin',
 			ssl: false
 		},
 		development: {
-			url: 'mongodb://dbapp:dbapp123@149.129.249.18:27017/s_images?authSource=admin',
+			url: 'mongodb://dbapp:dbapp123@149.129.249.18:27017/s_report?authSource=admin',
 			ssl: false
 		}
 	}
