3f5ed5a755917e09c3b35861eec555cd35afe66d
ferdinand12345 <ferdshinodas@gmail.com>
2019-06-14 16:13:38 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@
 |--------------------------------------------------------------------------
 */
 	module.exports = {
-		production: {
+		/*production: {
 			url: 'mongodb://dbapp:dbapp123@dbapp.tap-agri.com:27017/s_report?authSource=admin',
 			ssl: false
-		},
+		},*/
 		development: {
 			url: 'mongodb://s_report:s_report@dbappdev.tap-agri.com:4848/s_report?authSource=s_report',
 			ssl: false
