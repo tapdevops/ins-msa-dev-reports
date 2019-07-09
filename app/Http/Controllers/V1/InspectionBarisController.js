1a8d48bffa413217d7f10f25d579fde0db27bd55
ferdinand12345 <ferdshinodas@gmail.com>
2019-07-09 10:20:02 +0700

    Ferdinand

--- app/Http/Controllers/V1/InspectionBarisController.js
+++ app/Http/Controllers/V1/InspectionBarisController.js
@@ -27,6 +27,80 @@
 
 		// Update Data lama
 		if ( query ) {
+			return res.send( {
+				status: true,
+				message: 'Data di skip, sudah ada di database.',
+				data: []
+			} );
+		}
+		else {
+			const set = new InspectionBarisSchema( {
+				BLOCK_INSPECTION_CODE: req.body.BLOCK_INSPECTION_CODE,
+				PERIODE: req.body.PERIODE,
+				WERKS_AFD_CODE: req.body.WERKS_AFD_CODE,
+				WERKS_AFD_BLOCK_CODE: req.body.WERKS_AFD_BLOCK_CODE,
+				WERKS: req.body.WERKS,
+				EST_NAME: req.body.EST_NAME,
+				AFD_CODE: req.body.AFD_CODE,
+				AFD_NAME: req.body.AFD_NAME,
+				BLOCK_CODE: req.body.BLOCK_CODE,
+				BLOCK_NAME: req.body.BLOCK_NAME,
+				LAT_START_INSPECTION: req.body.LAT_START_INSPECTION,
+				LONG_START_INSPECTION: req.body.LAT_START_INSPECTION,
+				INSPECTION_DATE: req.body.INSPECTION_DATE,
+				INSPECTION_TIME: req.body.INSPECTION_TIME,
+				AREAL: req.body.AREAL,
+				LAMA_INSPEKSI: req.body.LAMA_INSPEKSI,
+				SPMON: req.body.SPMON,
+				MATURITY_STATUS: req.body.MATURITY_STATUS,
+				REPORTER_FULLNAME: req.body.REPORTER_FULLNAME,
+				REPORTER_JOB: req.body.REPORTER_JOB,
+				REPORTER_REF_ROLE: req.body.REPORTER_REF_ROLE,
+				REPORTER_USER_ROLE: req.body.REPORTER_USER_ROLE,
+				REPORTER_USER_AUTH_CODE: req.body.REPORTER_USER_AUTH_CODE,
+				REPORTER_NIK: req.body.REPORTER_NIK,
+				CONTENT: req.body.CONTENT,
+				CONTENT_PANEN: req.body.CONTENT_PANEN,
+				CONTENT_PEMUPUKAN: req.body.CONTENT_PEMUPUKAN,
+				CONTENT_PERAWATAN: req.body.CONTENT_PERAWATAN,
+				INSERT_TIME: Date.convert( 'now', 'YYYYMMDDHHmmss' )
+			} );
+
+			set.save()
+			.then( data => {
+				if ( !data ) {
+					return res.send( {
+						status: true,
+						message: 'Error!',
+						data: []
+					} );
+				}
+				
+				return res.send( {
+					status: true,
+					message: 'Success!',
+					data: []
+				} );
+			} ).catch( err => {
+				return res.send( {
+					status: true,
+					message: 'Error!',
+					data: []
+				} );
+			} );
+		}
+	}
+
+	exports.backup_create_or_update_v_1_0 = async ( req, res ) => {
+
+		var query = await InspectionBarisSchema.findOne( {
+			BLOCK_INSPECTION_CODE: req.body.BLOCK_INSPECTION_CODE,
+			WERKS_AFD_BLOCK_CODE: req.body.WERKS_AFD_BLOCK_CODE,
+			INSPECTION_DATE: req.body.INSPECTION_DATE
+		} );
+
+		// Update Data lama
+		if ( query ) {
 			var update_body = {
 				PERIODE: req.body.PERIODE,
 				EST_NAME: req.body.EST_NAME,
